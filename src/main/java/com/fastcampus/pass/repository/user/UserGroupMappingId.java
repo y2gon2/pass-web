15ec6bb3c12171eb496f7091b6602d77279d05fa
gon <y2gon2@gmail.com>
2023-11-05 16:18:21 +0900

    #3 - UserGroupMappingEntitty 복합키 (다중 id) 사용에  대한 id 일치 여부 확인에 필요한 equals() 와 hashcode() override
    현재 직접적으로 UserGroupMappingEntity 의 record 을 비교하는 code 를 구현한 것은 없다. 그런데 아마도 UserGroupMappingRepository 에서 Query 문으로 필요한 record 를 찾을 대 동치 여부를 판단하는 기준이 필요한듯 보인다.
    
    이를 위해서는  복합키를 정의한 UserGroupMappingId 에서 두 복합키에 대한 equals() 와 hashcode() override 구현
    
    ## equals()와 hashCode()  method  override 의 필요성 (by ChatGPT)
    
    equals()와 hashCode() 메서드는 자바의 모든 객체에 대한 기본 메서드입니다. 이 메서드들은 객체의 동등성을 비교하고, 객체가 해시 기반 컬렉션(예: HashSet, HashMap, HashTable)에 들어갈 때 객체의 해시 값을 계산하는 데 사용됩니다.
    
    앞에서 정의한 UserGroupMappingEntity가 복합 키를 사용하는 엔티티인 경우, equals()와 hashCode() 메서드는 엔티티의 인스턴스 동등성을 처리하는데 매우 중요합니다. JPA와 같은 ORM(Object-Relational Mapping) 프레임워크에서는 이 메서드들이 엔티티의 동일성(identity)을 판단하는 데 사용될 수 있습니다.
    
    당신이 제시한 UserGroupMappingRepository 인터페이스의 메서드 findDistinctUserGroupId()는 복합 키와 관련된 비교 작업을 직접 수행하지 않고, userGroupId 필드를 기준으로 데이터를 조회합니다. 이 경우, equals()와 hashCode()의 구현 유무는 이 특정 쿼리의 동작에 직접적인 영향을 주지 않을 것입니다.
    
    하지만, equals()와 hashCode() 구현이 없는 경우에 발생할 수 있는 문제는 다음과 같습니다:
    
    해시 기반 컬렉션 사용: 만약 UserGroupMappingEntity 인스턴스를 해시 기반 컬렉션에 저장하거나, 이러한 컬렉션의 키로 사용하는 경우, 올바른 equals()와 hashCode() 구현이 없다면 예상치 못한 동작이 발생할 수 있습니다. 예를 들어, 두 엔티티 인스턴스가 논리적으로 동일한 복합 키 값을 가지고 있음에도 불구하고, 이들이 다른 해시 코드를 가질 수 있고, 따라서 같은 엔티티로 인식되지 않을 수 있습니다.
    
    캐싱 메커니즘: JPA는 내부적으로 1차 캐시(first-level cache)를 사용하여, 같은 트랜잭션 내에서 동일한 엔티티 조회 시에 데이터베이스로부터의 추가적인 조회를 방지합니다. equals()와 hashCode()가 올바르게 구현되어 있지 않으면, 엔티티의 동일성 비교에서 문제가 발생할 수 있습니다.
    
    엔티티 간의 관계 매핑: 엔티티 간의 관계(예: @OneToMany, @ManyToMany)를 매핑할 때, 복합 키를 가진 엔티티의 동등성 비교가 필요할 수 있습니다. 올바른 equals()와 hashCode() 구현이 없다면, JPA가 관계를 관리하는데 어려움을 겪을 수 있습니다.
    
    따라서, 복합 키를 사용할 때 equals()와 hashCode()를 명시적으로 구현하는 것은 엔티티의 정확한 동작을 보장하기 위해 매우 중요합니다. 비록 현재 구현한 리포지토리 메서드에서 직접적으로 문제가 발생하지 않더라도, 나중에 애플리케이션이 확장되거나 리팩토링하는 과정에서 문제가 발생할 가능성이 있습니다.

--- src/main/java/com/fastcampus/pass/repository/user/UserGroupMappingId.java
+++ src/main/java/com/fastcampus/pass/repository/user/UserGroupMappingId.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -12,4 +13,18 @@ import java.io.Serializable;
 public class UserGroupMappingId implements Serializable {
     private String userGroupId;
     private String userId;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        UserGroupMappingId that = (UserGroupMappingId) obj;
+        return Objects.equals(this.getUserGroupId(), that.getUserGroupId())
+                && Objects.equals(this.getUserId(), that.getUserId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getUserGroupId(), this.getUserId());
+    }
 }
